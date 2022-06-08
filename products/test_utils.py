f56345d2ab1e5c9dad1741bbd0c441f94b09d52c
Oliver Cadman <o.cadman@live.co.uk>
2022-06-08 21:56:05 +0100

    Created function to create test user

--- products/test_utils.py
+++ products/test_utils.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 def get_test_form_data():
     data = {
             "name": "test product",
@@ -11,3 +13,17 @@ def get_test_form_data():
         }
     
     return data
+
+def create_test_user(username, password, email):
+
+    test_username = username
+    test_password = password
+    test_email = email
+
+    user = get_user_model().objects.create_user(
+        username=test_username,
+        password=test_password,
+        email=test_email
+    )
+
+    return user
