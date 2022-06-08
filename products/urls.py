f2de3ef8dbf0b02ab275f2a6dc8aba7dacbe1fe7
Oliver Cadman <o.cadman@live.co.uk>
2022-06-08 21:56:18 +0100

    Add pagination to product list

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.add_product_view, name="add_product"),
-    path("product_list/", views.product_list_view, name="product_list")
+    path("product_list/", views.product_list_view, name="product_list"),
+    path("edit_product/<int:pk>", views.product_edit_view, name="edit_product")
 ]
