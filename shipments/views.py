1b74d89a5537af2f2f7584047270fc29eaba2b50
Oliver Cadman <o.cadman@live.co.uk>
2022-06-09 20:40:47 +0100

    Create view to create a shipment

--- shipments/views.py
+++ shipments/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Shipment
 
-# Create your views here.
+
+class CreateShipmentView(CreateView):
+    template_name = "shipments/shipment_form.html"
+    model = Shipment
+    fields = [
+        "recipient_name",
+        "email",
+        "phone_number",
+        "street_address1",
+        "street_address2",
+        "town_or_city",
+        "county",
+        "country",
+        "postcode"
+    ]
+
+create_shipment_view = CreateShipmentView.as_view()
