bef4eb064151b3d0948753eaa3ce88d213e67b6e
Oliver Cadman <o.cadman@live.co.uk>
2022-06-13 09:35:59 +0100

    Added all products to shipment form, with forms to add quantity of product to shipment

--- shipments/views.py
+++ shipments/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 from django.views.generic import CreateView
+from django.forms import modelformset_factory
 from .models import Shipment
+from products.models import Product
 
 
 class CreateShipmentView(CreateView):
@@ -17,5 +20,16 @@ class CreateShipmentView(CreateView):
         "country",
         "postcode"
     ]
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        all_products = Product.objects.all()
+        context["products"] = all_products
+        return context
+    
+    def post(self, *args, **kwargs):
+        print("POST", self.request)
+        return super().post(*args, **kwargs)
+    
 
 create_shipment_view = CreateShipmentView.as_view()
