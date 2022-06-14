49bbeaeb191aa90fb9d94e730d93d5dacb6122fd
Oliver Cadman <o.cadman@live.co.uk>
2022-06-14 11:21:45 +0100

    Added URL to add_to_shipment view

--- shipments/urls.py
+++ shipments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create_shipment/", views.create_shipment_view, name="create_shipment")
+    path("create_shipment/", views.create_shipment, name="create_shipment"),
+    path("add_to_shipment/<int:product_id>", views.add_to_shipment, name="add_to_shipment")
 ]
