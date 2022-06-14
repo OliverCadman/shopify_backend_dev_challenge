6d806998d1135ec459b22ef2b6f8739c2fe1cf21
Oliver Cadman <o.cadman@live.co.uk>
2022-06-14 11:16:54 +0100

    Registered Shipment and ShipmentLineItem models with admin

--- shipments/admin.py
+++ shipments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Shipment, ShipmentLineItem
+
 
 # Register your models here.
+admin.site.register(Shipment)
+admin.site.register(ShipmentLineItem)
